1fe2540f641e08bb09e50f43f7882664f4311445
Pavankalyan5116 <pawankalyanvarikuppala2276@gmail.com>
2025-07-13 11:04:14 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -5,7 +5,7 @@ import os
 from langchain_google_genai import GoogleGenerativeAI as ai
 
 # Set up API key securely
-os.environ["GOOGLE_API_KEY"] = "AIzaSyD4D82n6QJqllwif14MojSQgEm1KnHa33s"
+os.environ["GOOGLE_API_KEY"] = "Your-Google-api-key"
 
 # Initialize LLM
 llm = ai(temperature=0, model="gemini-2.0-flash")
